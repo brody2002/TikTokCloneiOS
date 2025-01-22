3475890a7c3d20e693722bfdc4c13fb3aaf27500
Brody Roberts <brodyroberts2002@gmail.com>
2025-01-22 09:06:05 -0800

    first commit of the TikTok Clone

---
+++ TikTokClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,70 @@
+//
+//  MainTabView.swift
+//  TikTokClone
+//
+//  Created by Brody on 1/22/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab: Int = 0
+    var body: some View {
+        TabView(selection: $selectedTab){
+            Text("Feed")
+                .tabItem {
+                    VStack{
+                        Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            Text("Friends")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Friends")
+                    }
+                }
+                .onAppear { selectedTab =  1}
+                .tag(1)
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
+            
+            Text("Inbox")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "ellipsis.message")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Inbox")
+                    }
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+            
+            Text("Profile")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Brody on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
